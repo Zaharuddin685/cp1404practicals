7b41b9e13fa6bd718aec73a51ae7526fa84f1587
zaharuddinyusli <zaharuddin.yusli@my.jcu.edu.au>
2025-02-23 11:50:55 +0800

    Added print income as its own function

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -7,17 +7,22 @@ Starter code for cumulative total income program
 def main():
     """Display income report for incomes over a given number of months."""
     incomes = []
-    months = int(input("How many months? "))
+    num_of_months = int(input("How many months? "))
 
-    for month in range(1, months + 1):
+    for month in range(1, num_of_months + 1):
         income = float(input(f"Enter income for month {month}: "))  # Updated to use an f-string
         incomes.append(income)
 
+    print_income_report(incomes, num_of_months)
+
+
+def print_income_report(incomes, num_of_months):
     print("\nIncome Report\n-------------")
     total = 0
-    for month in range(1, months + 1):
+    for month in range(1, num_of_months + 1):
         income = incomes[month - 1]
         total += income
         print(f"Month {month:2} - Income: ${income:10.2f} Total: ${total:10.2f}")  # Also updated to use f-string
+
 
 main()
