bb1b83274278384c99fdfb8a1b7fe8910538fbc9
zaharuddinyusli <zaharuddin.yusli@my.jcu.edu.au>
2025-04-04 15:32:00 +0800

    Added programmes

---
+++ prac_09/taxi_test.py
@@ -0,0 +1,24 @@
+from taxi import Taxi
+
+def main():
+    # Create a new taxi without passing price_per_km
+    my_taxi = Taxi("Prius 1", 100)
+
+    # Drive 40 km
+    my_taxi.drive(40)
+
+    # Print taxi's details and the current fare
+    print(my_taxi)
+    print(f"Current fare: ${my_taxi.get_fare():.2f}")
+
+    # Start a new fare
+    my_taxi.start_fare()
+
+    # Drive 100 km
+    my_taxi.drive(100)
+
+    # Print taxi's details and the current fare
+    print(my_taxi)
+    print(f"Current fare: ${my_taxi.get_fare():.2f}")
+
+main()
